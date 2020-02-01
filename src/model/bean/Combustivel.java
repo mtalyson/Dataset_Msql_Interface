bd7ed1c159dd25e2ad4c43d74626b52c0f2cccd1
Talyson Moreira <talyson.penha@gmail.com>
2020-02-01 19:37:04 -0300

    #1 Commit

---
+++ src/model/bean/Combustivel.java
@@ -0,0 +1,204 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+/**
+ *
+ * @author Talyson
+ */
+public class Combustivel {
+    
+    private String idColuna;
+    private String dataInicio;
+    private String dataFinal;
+    private String regiao;
+    private String estado;
+    private String produto;
+    private String numeroPostos;
+    private String unidadeMedida;
+    private String precoMedioRevenda;
+    private String desvioPadraoRevenda;
+    private String precoMinimoRevenda;
+    private String precoMaximoRevenda;
+    private String margemMediaRevenda;
+    private String coefVariacaoRevenda;
+    private String precoMedioDistribuicao;
+    private String desvioPadraoDistribuicao;
+    private String precoMinimoDistribuicao;
+    private String precoMaximoDistribuicao;
+    private String coefVariacaoDistribuicao;
+    private String mes;
+    private String ano;
+
+    public String getIdColuna() {
+        return idColuna;
+    }
+
+    public void setIdColuna(String idColuna) {
+        this.idColuna = idColuna;
+    }
+
+    public String getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(String dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public String getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(String dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+    public String getRegiao() {
+        return regiao;
+    }
+
+    public void setRegiao(String regiao) {
+        this.regiao = regiao;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
+
+    public String getNumeroPostos() {
+        return numeroPostos;
+    }
+
+    public void setNumeroPostos(String numeroPostos) {
+        this.numeroPostos = numeroPostos;
+    }
+
+    public String getUnidadeMedida() {
+        return unidadeMedida;
+    }
+
+    public void setUnidadeMedida(String unidadeMedida) {
+        this.unidadeMedida = unidadeMedida;
+    }
+
+    public String getPrecoMedioRevenda() {
+        return precoMedioRevenda;
+    }
+
+    public void setPrecoMedioRevenda(String precoMedioRevenda) {
+        this.precoMedioRevenda = precoMedioRevenda;
+    }
+
+    public String getDesvioPadraoRevenda() {
+        return desvioPadraoRevenda;
+    }
+
+    public void setDesvioPadraoRevenda(String desvioPadraoRevenda) {
+        this.desvioPadraoRevenda = desvioPadraoRevenda;
+    }
+
+    public String getPrecoMinimoRevenda() {
+        return precoMinimoRevenda;
+    }
+
+    public void setPrecoMinimoRevenda(String precoMinimoRevenda) {
+        this.precoMinimoRevenda = precoMinimoRevenda;
+    }
+
+    public String getPrecoMaximoRevenda() {
+        return precoMaximoRevenda;
+    }
+
+    public void setPrecoMaximoRevenda(String precoMaximoRevenda) {
+        this.precoMaximoRevenda = precoMaximoRevenda;
+    }
+
+    public String getMargemMediaRevenda() {
+        return margemMediaRevenda;
+    }
+
+    public void setMargemMediaRevenda(String margemMediaRevenda) {
+        this.margemMediaRevenda = margemMediaRevenda;
+    }
+
+    public String getCoefVariacaoRevenda() {
+        return coefVariacaoRevenda;
+    }
+
+    public void setCoefVariacaoRevenda(String coefVariacaoRevenda) {
+        this.coefVariacaoRevenda = coefVariacaoRevenda;
+    }
+
+    public String getPrecoMedioDistribuicao() {
+        return precoMedioDistribuicao;
+    }
+
+    public void setPrecoMedioDistribuicao(String precoMedioDistribuicao) {
+        this.precoMedioDistribuicao = precoMedioDistribuicao;
+    }
+
+    public String getDesvioPadraoDistribuicao() {
+        return desvioPadraoDistribuicao;
+    }
+
+    public void setDesvioPadraoDistribuicao(String desvioPadraoDistribuicao) {
+        this.desvioPadraoDistribuicao = desvioPadraoDistribuicao;
+    }
+
+    public String getPrecoMinimoDistribuicao() {
+        return precoMinimoDistribuicao;
+    }
+
+    public void setPrecoMinimoDistribuicao(String precoMinimoDistribuicao) {
+        this.precoMinimoDistribuicao = precoMinimoDistribuicao;
+    }
+
+    public String getPrecoMaximoDistribuicao() {
+        return precoMaximoDistribuicao;
+    }
+
+    public void setPrecoMaximoDistribuicao(String precoMaximoDistribuicao) {
+        this.precoMaximoDistribuicao = precoMaximoDistribuicao;
+    }
+
+    public String getCoefVariacaoDistribuicao() {
+        return coefVariacaoDistribuicao;
+    }
+
+    public void setCoefVariacaoDistribuicao(String coefVariacaoDistribuicao) {
+        this.coefVariacaoDistribuicao = coefVariacaoDistribuicao;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+    
+}
---
+++ src/model/dao/ProdutoDAO.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao;
+
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import model.bean.Combustivel;
+
+/**
+ *
+ * @author Talyson
+ */
+public class ProdutoDAO {
+    
+    public void create(Combustivel p){
+
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        
+        try {
+            stmt = con.prepareStatement("INSERT INTO combustivel(idColuna,dataInicio,dataFinal,regiao,"
+                    + "estado,produto,numeroPostos,unidadeMedida,precoMedioRevenda,desvioPadraoRevenda,"
+                    + "precoMinimoRevenda,precoMaximoRevenda,margemMediaRevenda,coefVariacaoRevenda,"
+                    + "precoMedioDistribuicao,desvioPadraoDistribuicao,precoMinimoDistribuicao,"
+                    + "precoMaximoDistribuicao,coefVariacaoDistribuicao,mes,ano) VALUES(?,?,?,?,?,?,"
+                    + "?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+            
+            stmt.setString(1,p.getIdColuna());
+            stmt.setString(2,p.getDataInicio());
+            stmt.setString(3,p.getDataFinal());
+            stmt.setString(4,p.getRegiao());
+            stmt.setString(5,p.getEstado());
+            stmt.setString(6,p.getProduto());
+            stmt.setString(7,p.getNumeroPostos());
+            stmt.setString(8,p.getUnidadeMedida());
+            stmt.setString(9,p.getPrecoMedioRevenda());
+            stmt.setString(10,p.getDesvioPadraoRevenda());
+            stmt.setString(11,p.getPrecoMinimoRevenda());
+            stmt.setString(12,p.getPrecoMaximoRevenda());
+            stmt.setString(13,p.getMargemMediaRevenda());
+            stmt.setString(14,p.getCoefVariacaoRevenda());
+            stmt.setString(15,p.getPrecoMedioDistribuicao());
+            stmt.setString(16,p.getDesvioPadraoDistribuicao());
+            stmt.setString(17,p.getPrecoMinimoDistribuicao());
+            stmt.setString(18,p.getPrecoMaximoDistribuicao());
+            stmt.setString(19,p.getCoefVariacaoDistribuicao());
+            stmt.setString(20,p.getMes());
+            stmt.setString(21,p.getAno());
+            
+            stmt.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Salvo com Sucesso!");
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao Salvar: " + ex);
+        }finally{
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
+    
+    public List<Combustivel> read(){
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;  
+        ResultSet rs = null;
+        
+        List<Combustivel> combustiveis = new ArrayList<>();
+        
+        try {
+            stmt = con.prepareStatement("SELECT * FROM combustivel");
+            rs = stmt.executeQuery();
+            
+            while (rs.next()){
+                
+                Combustivel combustivel  = new Combustivel();
+                
+                combustivel.setIdColuna(rs.getString("idColuna"));
+                combustivel.setDataInicio(rs.getString("dataInicio"));
+                combustivel.setDataFinal(rs.getString("dataFinal"));
+                combustivel.setRegiao(rs.getString("regiao"));
+                combustivel.setEstado(rs.getString("estado"));
+                combustivel.setProduto(rs.getString("produto"));
+                combustivel.setNumeroPostos(rs.getString("numeroPostos"));
+                combustivel.setUnidadeMedida(rs.getString("unidadeMedida"));
+                combustivel.setPrecoMedioRevenda(rs.getString("precoMedioRevenda"));
+                combustivel.setDesvioPadraoRevenda(rs.getString("desvioPadraoRevenda"));
+                combustivel.setPrecoMinimoRevenda(rs.getString("precoMinimoRevenda"));
+                combustivel.setPrecoMaximoRevenda(rs.getString("precoMaximoRevenda"));
+                combustivel.setMargemMediaRevenda(rs.getString("margemMediaRevenda"));
+                combustivel.setCoefVariacaoRevenda(rs.getString("coefVariacaoRevenda"));
+                combustivel.setPrecoMedioDistribuicao(rs.getString("precoMedioDistribuicao"));
+                combustivel.setDesvioPadraoDistribuicao(rs.getString("desvioPadraoDistribuicao"));
+                combustivel.setPrecoMinimoDistribuicao(rs.getString("precoMinimoDistribuicao"));
+                combustivel.setPrecoMaximoDistribuicao(rs.getString("precoMaximoDistribuicao"));
+                combustivel.setCoefVariacaoDistribuicao(rs.getString("coefVariacaoDistribuicao"));
+                combustivel.setMes(rs.getString("mes"));
+                combustivel.setAno(rs.getString("ano"));
+                
+                combustiveis.add(combustivel);
+                
+            }
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao Exibir: " + ex);
+        }finally{
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+        
+        return combustiveis;
+    }
+    
+}
